a1c3640ec84a8b03aa9bb288b8fa17a25ea54c83
Viktoriia Dorzhieva <viktoria1dorzhieva@gmail.com>
2025-10-28 23:06:02 -0700

    Initial Commit

---
+++ FrenchVerbs/FrenchVerbsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchVerbsApp.swift
+//  FrenchVerbs
+//
+//  Created by Viktoriia Dorzhieva on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FrenchVerbsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrenchVerbsTests/FrenchVerbsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchVerbsTests.swift
+//  FrenchVerbsTests
+//
+//  Created by Viktoriia Dorzhieva on 10/28/25.
+//
+
+import Testing
+@testable import FrenchVerbs
+
+struct FrenchVerbsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
